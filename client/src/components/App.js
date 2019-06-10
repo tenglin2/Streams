b1b2972f041b1bcb3f1ccd0b0d93b715524418c0
Teng Lin <tenglin2@gmail.com>
2019-06-09 23:49:11 -0400

    sandbox with react-router-dom package

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        This is the app component
-      </div>
-    );
-  }
-}
+
+const PageOne = () => {
+  return (
+    <div>PageOne</div>
+  );
+};
+const PageTwo = () => {
+  return (
+    <div>page two material</div>
+  );
+};
+
+const App = function() {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={PageOne} />
+          <Route path='/pagetwo' component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
+
+// We need to nest the App inside of a BrowserRouter which we usually nest in a BrowserRouter tag.
+// React native also has a react-router-native which basically does the exact same thing.
+// Also ther is react-router-redux which is not really necessary. Similar to the other package of react-redux. Not recommended by anyone.
+
+// The react router dom package does not show up in the package.json file for some reason, but it does auto complete on import.
+
+// It looks like the problem was the directory that I was in. The root folder does not have npm intialized but the client folder already comes with create-react-app.
