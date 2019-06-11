973f606524dfdc023d9f2c2a05092af722a8e61a
Teng Lin <tenglin2@gmail.com>
2019-06-11 18:13:11 -0400

    redux testing

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions/actions';
 
+
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
