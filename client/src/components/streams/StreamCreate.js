b59b51689e87976eb2d59a1380d6fea6fb9b72ec
Teng Lin <tenglin2@gmail.com>
2019-06-11 23:54:27 -0400

    used redux forms to create basic form for creating streams with error handling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = function() {
-  return (
-    <div>
-      <p>Stream create block disply</p>
-      <p>SHOW UP PLEASE</p>
-    </div>
-  );
+class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+  
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input { ...input } autoComplete='off' />
+        {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+        <Field name='title' component={this.renderInput} label='Enter Title' />
+        <Field name='description' component={this.renderInput} label='Enter Description' />
+
+        <button className='ui button primary'>Submit</button>
+      </form>
+    );
+  }
 };
 
-export default StreamCreate;
+const validate = (formValues) => {
+  const errors = {};
+  
+  if (!formValues.title) {
+    // validate the title to check if there is a value.
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,7 +1,9 @@
 // You are naming this specifically. Remember to import it correctly.
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
