69c4de06c2d30cae7be6a3cad6a771a9ac09df31
Teng Lin <tenglin2@gmail.com>
2019-06-14 20:37:31 -0400

    edits to stream list

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions/actions';
 
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
   
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>EDITTO</button>
+          <button className='ui button negative'>DELETTO</button>
+        </div>
+      );
+    }
+  }
+  
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className='item' key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className='large middle aligned icon camera' />
           <div className='content'>
             {stream.title}
@@ -21,6 +34,16 @@ class StreamList extends React.Component {
       );
     });
   }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to='/streams/new' className='ui button primary'>Create a Stream</Link>
+        </div>
+      );
+    }
+  }
   
   render() {
     console.log(this.props.streams);
@@ -28,13 +51,18 @@ class StreamList extends React.Component {
       <div>
         <h2>Streamy Streams</h2>
         <div className='ui celled list'>{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return { 
+    streams: Object.values(state.streams), 
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
